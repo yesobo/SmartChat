4618824a8917e76de87d5fb224e919291976fe99
Jesus Espejo <yesobo@Jesuss-MacBook-Pro.local>
2020-09-27 21:08:37 +0200

    feat(dev): Bundle with webpack

--- server.js
+++ server.js
@@ -1,28 +1,32 @@
-var credentials = require('./credentials.json');
-var express = require('express');
-var TokenProvider = require('./lib/tokenprovider');
+var credentials = require("./credentials.json");
+var express = require("express");
+var TokenProvider = require("./lib/tokenprovider");
 
 var app = new express();
 var tokenProvider = new TokenProvider(credentials);
 
 if (credentials.authToken) {
-  console.warn('WARNING: The "authToken" field is deprecated. Please use "signingKeySecret".');
+  console.warn(
+    'WARNING: The "authToken" field is deprecated. Please use "signingKeySecret".'
+  );
 }
 
 if (credentials.instanceSid) {
-  console.warn('WARNING: The "instanceSid" field is deprecated. Please use "serviceSid".');
+  console.warn(
+    'WARNING: The "instanceSid" field is deprecated. Please use "serviceSid".'
+  );
 }
 
-app.get('/getToken', function(req, res) {
+app.get("/getToken", function (req, res) {
   var identity = req.query && req.query.identity;
   if (!identity) {
-    res.status(400).send('getToken requires an Identity to be provided');
+    res.status(400).send("getToken requires an Identity to be provided");
   }
 
   var token = tokenProvider.getToken(identity);
   res.send(token);
 });
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/dist"));
 
 app.listen(8080);
