3388ddab924149e88b2bf3d0b0304b327cafdfb1
yesobo <jesus.espejo.arnas@gmail.com>
2020-10-16 19:11:24 +0200

    css(messages): approach to whatsapp UI

--- src/message.js
+++ src/message.js
@@ -8,18 +8,16 @@ function isSent(message, identity) {
   return message.author == identity;
 }
 
-function createMessage(message, identity) {
-  var $el = $(
-    `<div class='message ${isSent(message, identity) ? "sent" : "received"}'/>`
-  ).attr("data-index", message.index);
-
+function renderRemoveBtn(message, $el) {
   var $remove = $('<div class="remove-button glyphicon glyphicon-remove"/>')
     .on("click", function (e) {
       e.preventDefault();
       message.remove();
     })
     .appendTo($el);
+}
 
+function renderEditBtn($el) {
   var $edit = $('<div class="remove-button glyphicon glyphicon-edit"/>')
     .on("click", function (e) {
       e.preventDefault();
@@ -29,58 +27,22 @@ function createMessage(message, identity) {
       $el.addClass("editing");
     })
     .appendTo($el);
+}
 
+function renderAvatar(message, $el) {
   var $img = $("<img/>")
     .attr(
       "src",
       "http://gravatar.com/avatar/" + MD5(message.author) + "?s=30&d=mm&r=g"
     )
     .appendTo($el);
+}
 
+function renderAuthor(message, $el) {
   var $author = $('<p class="author"/>').text(message.author).appendTo($el);
+}
 
-  var time = message.timestamp;
-  var minutes = time.getMinutes();
-  var ampm = Math.floor(time.getHours() / 12) ? "PM" : "AM";
-
-  if (minutes < 10) {
-    minutes = "0" + minutes;
-  }
-
-  var $timestamp = $('<span class="timestamp"/>')
-    .text("(" + (time.getHours() % 12) + ":" + minutes + " " + ampm + ")")
-    .appendTo($author);
-
-  if (message.lastUpdatedBy) {
-    time = message.dateUpdated;
-    minutes = time.getMinutes();
-    ampm = Math.floor(time.getHours() / 12) ? "PM" : "AM";
-
-    if (minutes < 10) {
-      minutes = "0" + minutes;
-    }
-
-    $('<span class="timestamp"/>')
-      .text(
-        "(Edited by " +
-          message.lastUpdatedBy +
-          " at " +
-          (time.getHours() % 12) +
-          ":" +
-          minutes +
-          " " +
-          ampm +
-          ")"
-      )
-      .appendTo($author);
-  }
-
-  var $body = $('<p class="body"/>').text(message.body).appendTo($el);
-
-  var $editBody = $('<textarea class="edit-body"/>')
-    .text(message.body)
-    .appendTo($el);
-
+function renderCancelEdit($el) {
   var $cancel = $('<button class="cancel-edit"/>')
     .text("Cancel")
     .on("click", function (e) {
@@ -91,19 +53,74 @@ function createMessage(message, identity) {
       $el.removeClass("editing");
     })
     .appendTo($el);
+}
 
+function renderEditedBy(message, time, minutes, ampm, $author) {
+  $('<span class="timestamp"/>')
+    .text(
+      "(Edited by " +
+        message.lastUpdatedBy +
+        " at " +
+        (time.getHours() % 12) +
+        ":" +
+        minutes +
+        " " +
+        ampm +
+        ")"
+    )
+    .appendTo($author);
+}
+
+function renderEditArea(message, $el) {
+  var $editBody = $('<textarea class="edit-body"/>')
+    .text(message.body)
+    .appendTo($el);
+}
+
+function renderConfirmEdit(message, $editBody, $el) {
   var $edit = $('<button class="red-button"/>')
     .text("Make Change")
     .on("click", function (e) {
       message.updateBody($editBody.val());
     })
     .appendTo($el);
+}
+
+function getMsgTimeStr(message) {
+  if (message.lastUpdatedBy) {
+    time = message.dateUpdated;
+  } else {
+    time = message.timestamp;
+  }
+  var minutes = time.getMinutes();
+  var ampm = Math.floor(time.getHours() / 12) ? "PM" : "AM";
+
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  return (time.getHours() % 12) + ":" + minutes + " " + ampm;
+}
+
+function createMessage(message, identity) {
+  var $el = $(
+    `<div class='message ${isSent(message, identity) ? "sent" : "received"}'/>`
+  ).attr("data-index", message.index);
+  $el.text(message.body);
+
+  var $metadata = $('<span class="metadata"/>');
+
+  var $timestamp = $('<span class="time"/>')
+    .text(getMsgTimeStr(message))
+    .appendTo($metadata);
+
+  $el.append($metadata);
 
   var $lastRead = $('<p class="last-read"/>')
     .text("New messages")
     .appendTo($el);
 
-  var $membersRead = $('<p class="members-read"/>').appendTo($el);
+  var $membersRead = $('<span class="tick"/>').appendTo($el);
   return $el;
 }
 
